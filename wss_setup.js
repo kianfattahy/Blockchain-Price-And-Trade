cdcbee68ca5f5b4712e95b367747dbde497ded00
Kian Fattahy <94335877+kianfattahy@users.noreply.github.com>
2021-12-16 17:07:26 -0500

    Delete wss_setup.js

--- wss_setup.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config({});
-const Web3 = require('web3');
-
-// loading env vars
-const HTTP_URL = process.env.HTTP_URL;
-const HTTP = new Web3(HTTP_URL);
-module.exports = { HTTP }
